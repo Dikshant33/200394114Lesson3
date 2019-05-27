5c3caef967111fb3272e2d2e12d2689e99913c72
Dikshant Dudhat <dixantdudhat33@gmail.com>
2019-05-27 18:18:47 -0400

    FinalLesson3

---
+++ Controllers/CarsController.cs
@@ -0,0 +1,40 @@
+﻿using _200394114Lesson3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace _200394114Lesson3.Controllers
+{
+    public class CarsController : Controller
+    {
+        List<Car> carList = new List<Car>()
+        {
+            new Car {id = 1, Brand ="Toyota", Model = "Corolla", Type ="Compact", Year = 2017 },
+            new Car {id = 2, Brand ="Ford", Model = "F-150", Type ="Truck", Year = 2018 },
+            new Car {id = 3, Brand ="Dodge", Model = "Carvan", Type ="Minivan", Year = 2019 }
+
+        };
+        // GET: Cars
+        public ActionResult Index()
+        {
+            return View(carList);
+        }
+        // Get car details
+        public ActionResult Details(int? id)
+        {
+            if(id == null || id > carList.Count)
+            {
+                return Content("Invaid car id");
+            }
+            var ind = Convert.ToInt32(id) - 1;
+            var car = carList[ind];
+
+            return View(car);
+        }
+    }
+
+    
+}
---
+++ Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _200394114Lesson3.Models
+{
+    public class Car
+    {
+        public int id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Type { get; set; }
+        public int Year { get; set; }
+    }
+}
